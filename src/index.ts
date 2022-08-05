2e6ce55a3b35a711ac3dcaa674925e51898f47fe
kauan777 <kauan7622@gmail.com>
2022-08-05 14:51:51 -0300

    add express and typescript settings

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('./', (req: Request, res: Response) => {
+    res.send("Hello world")
+})
+
+app.listen(3001, () => {
+    console.log("Funcionando!!")
+})
