4c323b7e7f09e8c4a0680bbdf6b98799d7ab100e
kauan777 <kauan7622@gmail.com>
2022-08-09 11:51:52 -0300

    add routes: login, signup

--- index.ts
+++ index.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express'
+import { loginRouter, signUpRouter } from './routes/player'
 
 const app = express()
 
 app.get('./', (req: Request, res: Response) => {
     res.send("Hello world")
 })
+
+app.use(express.json())
+
+app.use("/login", loginRouter);
+app.use("/signup", signUpRouter);
 
 app.listen(3001, () => {
     console.log("Funcionando!!")
