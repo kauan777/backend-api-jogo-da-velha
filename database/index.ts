3b0657ba43581834f723033b15b95ab6216fabf0
kauan777 <kauan7622@gmail.com>
2022-08-09 10:16:34 -0300

    add database

---
+++ database/index.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from "sequelize";
+
+const database =  new Sequelize(`db_jogo_da_velha`, `root`, `${process.env.DB_PASSWORD}`, {
+    dialect: 'mysql',
+    host: `${process.env.DB_HOST}`
+})
+
+export default database
