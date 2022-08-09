a65417c7ecda8f6d6efbdbf20eb3b31cc1a6d364
kauan777 <kauan7622@gmail.com>
2022-08-09 14:22:48 -0300

    starting to write function: login, signup

--- controllers/Player.ts
+++ controllers/Player.ts
@@ -2,14 +2,30 @@ import { Request, Response } from "express";
 import Player from "../models/playerModel";
 
 
+
 export const loginPlayer = async (req: Request, res: Response) => {
+  try {
+    const player = await Player.findAll({
+      where: {
+        email_player: req.body.email,
+        password_player: req.body.password,
+      },
+    });
+    if (res.status(200) && player[0] == null){
+      res.json({ message: "Usuario não encontrado" });
+      return;
+    }
+    const { nickname_player, total_victory }: any = player[0];
 
-    res.json({teste: "Usuario logado"})
-
-}
+    res.json({
+        nickname: nickname_player,
+        total_victory: total_victory
+    });
+  } catch (err: any) {
+    res.json({ message: err.message });
+  }
+};
 
 export const signUpPlayer = async (req: Request, res: Response) => {
-
-    res.json({teste: "Usuario cadastrado"})
-
-}
+  res.json({ teste: "Usuario cadastrado" });
+};
