a6c6b406284f9013f6f15f29113995fd30ef7f9f
kauan777 <kauan7622@gmail.com>
2022-09-17 18:46:04 -0300

    add cors

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 require('dotenv').config();
 
 
-const database =  new Sequelize(`db_jogo_da_velha`, `root`, `${process.env.DB_PASSWORD}`, {
+const database = new Sequelize(`db_jogo_da_velha`, `root`, `${process.env.DB_PASSWORD}`, {
     dialect: 'mysql',
     host: `${process.env.DB_HOST}`
 })
--- index.ts
+++ index.ts
@@ -1,16 +1,18 @@
-import express, { Request, Response } from 'express'
-import routerPlayer from './routes/player'
-import 'dotenv/config'
-const app = express()
+import express, { Request, Response } from "express";
+import routerPlayer from "./routes/player";
+import cors from "cors";
+import "dotenv/config";
+const app = express();
 
-app.get('./', (req: Request, res: Response) => {
-    res.send("Hello world")
-})
+app.get("./", (req: Request, res: Response) => {
+  res.send("Hello world");
+});
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.use("/", routerPlayer);
 
 app.listen(3001, () => {
-    console.log("Funcionando!!")
-})
+  console.log("Funcionando!!");
+});
